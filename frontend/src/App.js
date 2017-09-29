2117256d6e77a86806a50dede5d16f48a4c4d6c3
rjotwani <ravi.jotwani@jakra.net>
2017-09-29 13:27:11 -0700

    headers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './8ball.png';
 import './App.css';
 import {eightBallContract} from './EthereumSetup';
-//import Web3 from 'web3';
 import web3 from './EthereumSetup';
 
 class App extends Component {
@@ -16,16 +15,6 @@ class App extends Component {
         this.execute = this.execute.bind(this);
     }
 
-    /*componentDidMount() {
-        var answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Do not count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
-
-        var answer = answers[Math.floor(Math.random()*answers.length)];
-        //eightBallContract.sendTo().call();
-        eightBallContract.transferFrom(0x5d1EB7D49b406d210726CD627266247F86b71157, 0xB94c53B0E67FABac3d97173482663Ef597D4174a, 1000000000000000).send();
-        //eightBallContract.sendTo().send()
-        this.setState({answer});
-    }*/
-
     execute(e) {
         var answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Do not count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
         var answers1 = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes."];
@@ -33,7 +22,6 @@ class App extends Component {
         var answer = answers1[Math.floor(Math.random()*answers1.length)];
         //var answer = answers[Math.floor(Math.random()*answers.length)];
 
-        //console.log(eightBallContract);
         //eightBallContract.transferFrom("0x069fd4784D1DEd8A63923e83fF73c44414240043");
         //eightBallContract.transferFrom("0x069fd4784D1DEd8A63923e83fF73c44414240043", {from: "0x5d1EB7D49b406d210726CD627266247F86b71157", value: 1000000000000000});
         this.setState({answer});
