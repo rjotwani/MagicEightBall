6afbc05bc6fcad2427914390fdaa0cd0a6440891
rjotwani <ravi.jotwani@jakra.net>
2017-09-29 13:19:21 -0700

    css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,27 +28,29 @@ class App extends Component {
 
     execute(e) {
         var answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Do not count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+        var answers1 = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes."];
 
-        var answer = answers[Math.floor(Math.random()*answers.length)];
-        console.log(eightBallContract);
+        var answer = answers1[Math.floor(Math.random()*answers1.length)];
+        //var answer = answers[Math.floor(Math.random()*answers.length)];
+
+        //console.log(eightBallContract);
         //eightBallContract.transferFrom("0x069fd4784D1DEd8A63923e83fF73c44414240043");
-        eightBallContract.transferFrom("0x069fd4784D1DEd8A63923e83fF73c44414240043", {from: "0x5d1EB7D49b406d210726CD627266247F86b71157", value: 1000000000000000});
+        //eightBallContract.transferFrom("0x069fd4784D1DEd8A63923e83fF73c44414240043", {from: "0x5d1EB7D49b406d210726CD627266247F86b71157", value: 1000000000000000});
         this.setState({answer});
     }
 
     render() {
         return (
+          <body>
             <div className="App">
-                <h1>Magic Eight Ball for Charity</h1>
-                <p>Enter your question:</p>
-                <p className="App-intro">
-                    <input type="text" />
-                    <br /><br /><button onClick={this.execute} type="submit">Donate 30 cents and Answer my Question!</button>
-                </p>
-                <p>
-                    {this.state.answer}
-                </p>
+                <img src={ logo } />
+                <h1>Magic Eight Ball</h1>
+                <p>Enter your question:<br />
+                <input type="text" /><br /><br />
+                <button onClick={this.execute} type="submit">Donate 30 cents and Answer my Question</button>
+                <br /><br /><div className="answer">{this.state.answer}</div></p>
             </div>
+          </body>
         );
     }
   }
